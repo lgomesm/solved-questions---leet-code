19c09e44634bb0c2acfc5e0fa30055b3f63c60cd
Lucas Gomes Morais <134981411+lgomesm@users.noreply.github.com>
2024-06-07 00:22:04 -0300

    Time: 102 ms (72.93%) | Memory: 46.8 MB (25.56%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] FinalPrices(int[] prices) {
+        int n = prices.Length;
+        int[] answer = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int discount = 0;
+            for (int j = i + 1; j < n; j++) {
+                if (prices[j] <= prices[i]) {
+                    discount = prices[j];
+                    break;
+                }
+            }
+            answer[i] = prices[i] - discount;
+        }
+        
+        return answer;
+    }
+}
